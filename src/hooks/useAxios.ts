9459f74582a7c28087bfd096acc972e349ab664b
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2023-06-21 11:19:21 +0100

    remove dist and update environment variable for api base url

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const useAxios = () => {
-    const baseURL = 'http://localhost:5000/api'
+    // const baseURL = 'http://localhost:5000/api'
+    const baseURL = import.meta.env.VITE_API_BASE_URL
 
     const axiosPublic = axios.create({ baseURL })
 
